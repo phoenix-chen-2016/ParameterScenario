bab7fabf50980ccbfa113e1446e56f79bdd6e58f
Phoenix <fphoenix923@hotmail.com>
2020-03-16 22:27:00 +0800

    建立ParametersXmlAddin，大小寫區分問題情境

---
+++ pack-fixed.ps1
@@ -0,0 +1,25 @@
+param(
+    $Configuration = 'Debug',
+    $OutputDir = '..\Publish\AutoAddlSGameApiList\'
+)
+
+$MsDeployPath = Get-ChildItem 'Registry::HKLM\SOFTWARE\Microsoft\IIS Extensions\MSDeploy' `
+    | sort -Property Name -Descending `
+    | select -First 1 `
+    | Get-ItemProperty -Name InstallPath `
+    | %{ $_.InstallPath }
+
+$MsDeployExe = "$($MsDeployPath)msdeploy.exe"
+
+if(!(Test-Path $OutputDir)){
+    mkdir $OutputDir
+}
+
+$PackagePath = Get-Item $OutputDir
+$PackageName = $PackagePath.DirectoryName
+
+& "$MsDeployExe" `
+    -verb:sync `
+    -source:"contentPath=`"$(pwd)\bin\$Configuration`"" `
+    -dest:"package=`"$(Join-Path -Path $PackagePath.FullName -ChildPath "$($PackageName).zip")`"" `
+    -declareParamFile="fixed-parameters.xml"
